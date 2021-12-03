623a81f027ea36d099c29444847428cde9960720
Overij <overij@gmail.com>
2021-12-03 08:59:13 +0200

    Day 3 Part 1

---
+++ d3p1.php
@@ -0,0 +1,21 @@
+<?php
+
+$inputs = file('input/d3.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$bitsByPos = [];
+
+for ($i = 0; $i < strlen($inputs[0]); $i++)
+{
+    $bitsByPos[] = array_map(function($row) use ($i)
+    {
+        return $row[$i];
+    }, $inputs);
+}
+
+$gamma = implode('', array_map(function ($bits)
+{
+    $count = array_count_values($bits);
+    return ($count[0] > $count[1]) ? 0 : 1;
+}, $bitsByPos));
+
+echo bindec($gamma) * (bindec($gamma) ^ bindec(str_repeat('1', strlen($gamma)))) . \PHP_EOL;
