93f63e73608d50e4ce242cf404d39704712fdf03
Overij <overij@gmail.com>
2021-12-09 11:00:54 +0200

    Day 5

---
+++ day05part1.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @link https://adventofcode.com/2021/day/5
+ */
+
+$inputs = file('input/d5.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$points = [];
+foreach ($inputs as $line)
+{
+    $pair = explode(' -> ', $line);
+    [$x1, $y1] = explode(',', $pair[0]);
+    [$x2, $y2] = explode(',', $pair[1]);
+
+    if ($x1 != $x2 && $y1 != $y2)
+    {
+        continue;
+    }
+    
+    foreach (range($x1, $x2) as $x)
+    {
+        foreach (range($y1, $y2) as $y)
+        {
+            $points[] = $x . ',' . $y;
+        }
+    }
+}
+
+$counts = array_count_values($points);
+echo count(array_filter($counts, fn($x) => $x > 1)) . \PHP_EOL;
